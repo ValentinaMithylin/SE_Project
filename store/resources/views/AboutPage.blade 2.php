464adfc4c7ff372d315dd6a58d1c1963e077879a
ValentinaMithylin <mithylin15@gmail.com>
2023-09-20 00:58:00 +0600

    store files

---
+++ store/resources/views/AboutPage.blade 2.php
@@ -0,0 +1,4 @@
+<h1>This is the Abouts Page</h1>
+
+<a href="{{url('/')}}">HOME</a><br>
+<a href="{{url('about')}}">ABOUT</a><br>
---
+++ store/resources/views/HomePage.blade 2.php
@@ -0,0 +1,6 @@
+<h1>This is the Home Page</h1>
+
+<a href="{{url('/')}}">HOME</a><br>
+<a href="{{url('about')}}">ABOUT</a><br>
+<a href="{{url('signup')}}">SIGN UP</a><br>
+<a href="{{url('login')}}">LOGIN</a><br>
